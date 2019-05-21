9b73231d081a9f0122de5a9125909dba04cd116a
mriveralopez <mrivera.lopez@outlook.com>
2019-05-21 14:21:00 -0600

    Horario 15%

--- app/src/main/java/sv/edu/ues/fia/gade/UsuarioNormal/Horario/HorarioInsertarActivity.java
+++ app/src/main/java/sv/edu/ues/fia/gade/UsuarioNormal/Horario/HorarioInsertarActivity.java
@@ -3,6 +3,8 @@ package sv.edu.ues.fia.gade.UsuarioNormal.Horario;
 import android.app.Activity;
 import android.os.Bundle;
 
+import sv.edu.ues.fia.gade.R;
+
 public class HorarioInsertarActivity extends Activity {
 
     @Override
