addf787cba03c7d30b6179fa1151ff65f38c5a84
mriveralopez <mrivera.lopez@outlook.com>
2019-05-21 23:23:12 -0600

    Escuela 20%

---
+++ app/src/main/java/sv/edu/ues/fia/gade/UsuarioNormal/Escuela/EscuelaConsultarActivity.java
@@ -0,0 +1,15 @@
+package sv.edu.ues.fia.gade.UsuarioNormal.Escuela;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sv.edu.ues.fia.gade.R;
+
+public class EscuelaConsultarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_escuela_consultar);
+    }
+}
---
+++ app/src/main/java/sv/edu/ues/fia/gade/UsuarioNormal/Escuela/EscuelaInsertarActivity.java
@@ -0,0 +1,15 @@
+package sv.edu.ues.fia.gade.UsuarioNormal.Escuela;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sv.edu.ues.fia.gade.R;
+
+public class EscuelaInsertarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_escuela_insertar);
+    }
+}
